c93736e98b8617dfda413f07231cd57510db87d9
Shawn Hurley <smhurley00@gmail.com>
2018-05-21 11:05:09 -0400

    handling error

--- pkg/stub/handler.go
+++ pkg/stub/handler.go
@@ -8,6 +8,7 @@ import (
 
 	"github.com/operator-framework/operator-sdk/pkg/sdk/handler"
 	"github.com/operator-framework/operator-sdk/pkg/sdk/types"
+	log "github.com/sirupsen/logrus"
 )
 
 func NewHandler() handler.Handler {
@@ -21,11 +22,14 @@ type Handler struct {
 func (h *Handler) Handle(ctx types.Context, event types.Event) error {
 	switch o := event.Object.(type) {
 	case *v1alpha1.Broker:
-		err := broker.Reconcile(o)
-		if err != nil {
-			fmt.Printf("error reconciling broker - %v", err)
+		if !event.Deleted {
+			err := broker.Reconcile(o)
+			if err != nil {
+				fmt.Printf("error reconciling broker - %v", err)
+			}
+			return err
 		}
-		return err
+		log.Infof("deleted event: for %v", o.GetName())
 	}
 	return nil
 }
